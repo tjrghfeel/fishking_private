fb186a91cba51b615e6e66fad09985c231d7d3b9
tjrgh <tjrghfeel@kaimobile.com>
2021-01-29 16:31:13 +0900

    sns, pass연동 실서버 테스트

--- src/main/java/com/tobe/fishking/v2/controller/HelloController.java
+++ src/main/java/com/tobe/fishking/v2/controller/HelloController.java
@@ -5,6 +5,7 @@ import com.tobe.fishking.v2.model.HelloResponseDto;
 import com.tobe.fishking.v2.model.NoNameDTO;
 import com.tobe.fishking.v2.model.auth.SignUpDto;
 import com.tobe.fishking.v2.service.HelloService;
+import com.tobe.fishking.v2.service.auth.MemberService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Api(tags = {"Hello Test"})
@@ -28,6 +31,8 @@ import java.io.IOException;
 public class HelloController {
     @Autowired
     HelloService helloService;
+    @Autowired
+    MemberService memberService;
 
     @ApiOperation(value = "Hello", notes = "Hello Test")
 
@@ -51,6 +56,53 @@ public class HelloController {
         helloService.noName();
 
         return "asbdsf";
+    }
+
+    @GetMapping("/naver")
+    public String naver(HttpServletResponse response) throws IOException {
+//        response.sendRedirect("http://www.naver.com");
+        String url = "https://nid.naver.com/oauth2.0/authorize?" +
+                "response_type=code" +
+                "&client_id=xQF6XDWPhMC665JO2kSq" +
+                "&redirect_uri=https://www.fishkingapp.com/v2/api/naverAuthCode" +
+                "&state=abcd";
+        String method = "POST";
+        Map<String,String> parameter = new HashMap<String, String>();
+
+        String responseForAccessCode = memberService.sendRequest(url,method,parameter,"");
+
+        return responseForAccessCode;
+    }
+
+    @GetMapping("/pass")
+    public String pass(HttpServletResponse response) throws IOException {
+//        response.sendRedirect("http://www.naver.com");
+        String url = "https://id.passlogin.com/oauth2/authorize?" +
+                "response_type=code" +
+                "&client_id=uWHHuitm5at159jXPlc5" +
+                "&redirect_uri=https://www.fishkingapp.com/v2/api/passAuthCode" +
+                "&state=abcd";
+        String method = "POST";
+        Map<String,String> parameter = new HashMap<String, String>();
+
+        String responseForAccessCode = memberService.sendRequest(url,method,parameter,"");
+
+        return responseForAccessCode;
+    }
+
+    @GetMapping("/kakao")
+    public String kakao(HttpServletResponse response) throws IOException {
+//        response.sendRedirect("http://www.naver.com");
+        String url = "https://kauth.kakao.com/oauth/authorize?" +
+                "response_type=code" +
+                "&client_id=f0685b27f74d3f456d396195ca40796e" +
+                "&redirect_uri=https://www.fishkingapp.com/v2/api/kakaoAuthCode";
+        String method = "GET";
+        Map<String,String> parameter = new HashMap<String, String>();
+
+        String responseForAccessCode = memberService.sendRequest(url,method,parameter,"");
+
+        return responseForAccessCode;
     }
 
 }
