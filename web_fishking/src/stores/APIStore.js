2383e01bf4fd3f856a4a417f9c365020ce59444a
Administrator <admin@example.com>
2021-01-29 16:16:34 +0900

    ok

--- web_fishking/src/stores/APIStore.js
+++ web_fishking/src/stores/APIStore.js
@@ -44,63 +44,88 @@ const APIStore = new (class {
   };
 
   _delete = (url, params, headers) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const response = await Http.request(
-          url,
-          "DELETE",
-          headers,
-          null,
-          params
-        );
-        resolve(response);
-      } catch (err) {
-        reject(err);
-      }
+    return new Promise((resolve, reject) => {
+      runInAction(async () => {
+        this.isLoading = true;
+        try {
+          const response = await Http.request(
+            url,
+            "DELETE",
+            headers,
+            null,
+            params
+          );
+          this.isLoading = false;
+          resolve(response);
+        } catch (err) {
+          this.isLoading = false;
+          reject(err);
+        }
+      });
     });
   };
 
   _post = (url, data, headers) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const response = await Http.request(url, "POST", headers, null, data);
-        resolve(response);
-      } catch (err) {
-        reject(err);
-      }
+    return new Promise((resolve, reject) => {
+      runInAction(async () => {
+        this.isLoading = true;
+        try {
+          const response = await Http.request(url, "POST", headers, null, data);
+          this.isLoading = false;
+          resolve(response);
+        } catch (err) {
+          this.isLoading = false;
+          reject(err);
+        }
+      });
     });
   };
 
   _put = (url, data, headers) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const response = await Http.request(url, "PUT", headers, null, data);
-        resolve(response);
-      } catch (err) {
-        reject(err);
-      }
+    return new Promise((resolve, reject) => {
+      runInAction(async () => {
+        this.isLoading = true;
+        try {
+          const response = await Http.request(url, "PUT", headers, null, data);
+          this.isLoading = false;
+          resolve(response);
+        } catch (err) {
+          this.isLoading = false;
+          reject(err);
+        }
+      });
     });
   };
 
   _put_upload = (url, form, headers) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const response = await Http.upload(url, "PUT", headers, form);
-        resolve(response);
-      } catch (err) {
-        reject(err);
-      }
+    return new Promise((resolve, reject) => {
+      runInAction(async () => {
+        this.isLoading = true;
+        try {
+          const response = await Http.upload(url, "PUT", headers, form);
+          this.isLoading = false;
+          resolve(response);
+        } catch (err) {
+          this.isLoading = false;
+          reject(err);
+        }
+      });
     });
   };
 
   _post_upload = (url, form, headers) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const response = await Http.upload(url, "POST", headers, form);
-        resolve(response);
-      } catch (err) {
-        reject(err);
-      }
+    return new Promise((resolve, reject) => {
+      runInAction(async () => {
+        this.isLoading = true;
+        try {
+          const response = await Http.upload(url, "POST", headers, form);
+          this.isLoading = false;
+          resolve(response);
+        } catch (err) {
+          this.isLoading = false;
+          reject(err);
+        }
+      });
     });
   };
 })();
