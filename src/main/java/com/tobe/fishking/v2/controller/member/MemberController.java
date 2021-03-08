9f6a45cbbd0c9fed4e3d148c8536897c5af36dee
tjrgh <tjrghfeel@kaimobile.com>
2021-03-08 20:59:26 +0900

    회원가입 api 메소드 수정 v2

--- src/main/java/com/tobe/fishking/v2/controller/member/MemberController.java
+++ src/main/java/com/tobe/fishking/v2/controller/member/MemberController.java
@@ -136,7 +136,9 @@ public class MemberController {
             @RequestParam(value = "memberId",required = false,defaultValue = "") Long Id,
             @RequestParam("email") String email,
             @RequestParam("pw") String pw,
-            @RequestParam("nickName") String nickName
+            @RequestParam("nickName") String nickName,
+            ModelMap model,
+            HttpSession session
     ) throws ResourceNotFoundException, IOException {
         SignUpDto dto = SignUpDto.builder()
                 .memberId(Id)
@@ -156,8 +158,8 @@ public class MemberController {
         String sRequestNumber = memberId.toString();        	// 요청 번호, 이는 성공/실패후에 같은 값으로 되돌려주게 되므로
         System.out.println("================\n test >>> sRequestNumber : "+sRequestNumber+"\n================");
         // 업체에서 적절하게 변경하여 쓰거나, 아래와 같이 생성한다.
-//        sRequestNumber = niceCheck.getRequestNO(sSiteCode);
-//        session.setAttribute("REQ_SEQ" , sRequestNumber);	// 해킹등의 방지를 위하여 세션을 쓴다면, 세션에 요청번호를 넣는다.
+        sRequestNumber = niceCheck.getRequestNO(sSiteCode);
+        session.setAttribute("REQ_SEQ" , sRequestNumber);	// 해킹등의 방지를 위하여 세션을 쓴다면, 세션에 요청번호를 넣는다.
 
         String sAuthType = "M";      	// 없으면 기본 선택화면, M: 핸드폰, C: 신용카드, X: 공인인증서
 
@@ -193,8 +195,8 @@ public class MemberController {
         else if( iReturn == -9)        {            sMessage = "입력 데이터 오류입니다.";        }
         else        {            sMessage = "알수 없는 에러 입니다. iReturn : " + iReturn;        }
 
-//        model.addAttribute("sMessage",sMessage);
-//        model.addAttribute("sEncData",sEncData);
+        model.addAttribute("sMessage",sMessage);
+        model.addAttribute("sEncData",sEncData);
 
         return "niceRequest";
     }
