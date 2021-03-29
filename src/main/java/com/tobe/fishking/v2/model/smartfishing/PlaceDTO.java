a15b902f4bd53e605392a55da7b82bd4263c53fd
lyktien <lyktien@kaimobile.com>
2021-03-29 11:37:16 +0900

    갯바위등록 수정

--- src/main/java/com/tobe/fishking/v2/model/smartfishing/PlaceDTO.java
+++ src/main/java/com/tobe/fishking/v2/model/smartfishing/PlaceDTO.java
@@ -15,19 +15,19 @@ import java.util.List;
 @ApiModel(description = "갯바위 등록")
 public class PlaceDTO {
 
-    @ApiModelProperty(example = "위도. ")
+    @ApiModelProperty(example = "위도. 필수값", required = true)
     private @Valid Double latitude;
-    @ApiModelProperty(example = "경도")
+    @ApiModelProperty(example = "경도. 필수값", required = true)
     private @Valid Double longitude;
-    @ApiModelProperty(example = "갯바위 명칭")
+    @ApiModelProperty(example = "갯바위 명칭. 필수값", required = true)
     private @Valid String name;
-    @ApiModelProperty(example = "시/도")
+    @ApiModelProperty(example = "시/도. 필수값", required = true)
     private @Valid String sido;
-    @ApiModelProperty(example = "시/군/구")
+    @ApiModelProperty(example = "시/군/구. 필수값", required = true)
     private @Valid String sigungu;
     @ApiModelProperty(example = "읍/면/동")
-    private @Valid String dong;
-    @ApiModelProperty(example = "주소")
+    private @Valid String dong = "";
+    @ApiModelProperty(example = "주소. 필수값", required = true)
     private @Valid String address;
     @ApiModelProperty(example = "평균수심")
     private @Valid Double averageDepth;
@@ -37,7 +37,7 @@ public class PlaceDTO {
     private @Valid String tideTime;
     @ApiModelProperty(example = "소개")
     private @Valid String introduce;
-    @ApiModelProperty(example = "공개여부 공개: true, 비공개: false")
+    @ApiModelProperty(example = "공개여부 공개: true, 비공개: false. 필수값", required = true)
     private @Valid Boolean isOpen;
 //    @ApiModelProperty(example = "포인트 리스트")
     private @Valid List<PlacePointDTO> points;
