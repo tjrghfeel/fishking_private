0f1d3ba0593ab15b28d740bf5d1086f7131744c5
devcodeits <application@devcodeits.com>
2021-04-01 10:12:56 +0900

    wversion

--- web_fishking/src/routes/index.js
+++ web_fishking/src/routes/index.js
@@ -32,7 +32,6 @@ const {
 export default inject("PageStore")(
   observer(
     withRouter(({ PageStore }) => {
-      window.wversion = "202103281942";
       useEffect(() => {
         window.addEventListener("message", (message) => {
           const data = message.data;
