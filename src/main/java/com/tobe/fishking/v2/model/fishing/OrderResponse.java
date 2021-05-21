6467bbffd2b98622e57845f02dcd486144359383
lyktien <lyktien@kaimobile.com>
2021-05-21 15:20:08 +0900

    결제아이디 테스트 아이디로 변경

--- src/main/java/com/tobe/fishking/v2/model/fishing/OrderResponse.java
+++ src/main/java/com/tobe/fishking/v2/model/fishing/OrderResponse.java
@@ -42,8 +42,8 @@ public class OrderResponse {
         this.interestType = "NONE";
 //        this.reply = "http://112.220.72.178:8083/payresult";
         this.reply = "https://fishkingapp.com/payresult";
-//        this.shopNumber = "2999199900";
-        this.shopNumber = "2018400001";
+        this.shopNumber = "2999199999";
+//        this.shopNumber = "2018400001";
 //        this.shopNumber = "2040700001";
         this.payMethod = payMethod;
     }
