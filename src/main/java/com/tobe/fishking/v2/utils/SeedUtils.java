db960225fe981a138aac1d8e09d88a1a7a4ea324
lyktien <lyktien@kaimobile.com>
2021-10-21 19:02:38 +0900

    암호화 키 iv 값 이동

--- src/main/java/com/tobe/fishking/v2/utils/SeedUtils.java
+++ src/main/java/com/tobe/fishking/v2/utils/SeedUtils.java
@@ -1,6 +1,8 @@
 package com.tobe.fishking.v2.utils;
 
 import com.tobe.fishking.v2.addon.kisaseed.KISA_SEED_CBC;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -11,16 +13,18 @@ import java.util.Base64.Decoder;
 /*
     암호화 클래스
  */
+@Component
 public class SeedUtils {
 
     private static final Charset UTF_8 = StandardCharsets.UTF_8;
-    private static final byte[] pbszUserKey = "testCrypt2020!@#".getBytes();
-    private static final byte[] pbszIV = "1234567890123456".getBytes();
+    private static byte[] pbszUserKey;
+    private static byte[] pbszIV;
 
     public static String encrypt(String rawMessage) {
         Encoder encoder = Base64.getEncoder();
 
         byte[] message = rawMessage.getBytes(UTF_8);
+        System.out.println(pbszUserKey);
         byte[] encryptedMessage = KISA_SEED_CBC.SEED_CBC_Encrypt(pbszUserKey, pbszIV, message, 0, message.length);
 
         return new String(encoder.encode(encryptedMessage), UTF_8);
@@ -33,5 +37,15 @@ public class SeedUtils {
         byte[] decryptedMessage = KISA_SEED_CBC.SEED_CBC_Decrypt(pbszUserKey, pbszIV, message, 0, message.length);
 
         return new String(decryptedMessage, UTF_8);
+    }
+
+    @Value("${encrypKey.key}")
+    public void setPbszUserKey(String value) {
+        pbszUserKey = value.getBytes();
+    }
+
+    @Value("${encrypKey.iv}")
+    public void setPbszIV(String value) {
+        pbszIV = value.getBytes();
     }
 }
