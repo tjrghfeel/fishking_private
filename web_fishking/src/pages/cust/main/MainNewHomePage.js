be7177a206b59e51677949fc459d5fdd0e6beb99
lyktien <lyktien@kaimobile.com>
2021-11-29 18:52:14 +0900

    홈페이지 상단 메뉴 제거

--- web_fishking/src/pages/cust/main/MainNewHomePage.js
+++ web_fishking/src/pages/cust/main/MainNewHomePage.js
@@ -551,22 +551,22 @@ export default inject(
                   />
                 </a>
               </form>
-              <div className="modal_btn">
-                <a href="#" className="menu-button">
-                  <svg
-                    className="menu-icon"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 100 100"
-                  >
-                    <g fill="none" fill-rule="evenodd" stroke="#979797">
-                      <path d="M13,26.5 L88,26.5"/>
-                      <path d="M13,50.5 L88,50.5"/>
-                      <path className="x_path" d="M13,50.5 L88,50.5"/>
-                      <path d="M13,74.5 L88,74.5"/>
-                    </g>
-                  </svg>
-                </a>
-              </div>
+              {/*<div className="modal_btn">*/}
+              {/*  <a href="#" className="menu-button">*/}
+              {/*    <svg*/}
+              {/*      className="menu-icon"*/}
+              {/*      xmlns="http://www.w3.org/2000/svg"*/}
+              {/*      viewBox="0 0 100 100"*/}
+              {/*    >*/}
+              {/*      <g fill="none" fill-rule="evenodd" stroke="#979797">*/}
+              {/*        <path d="M13,26.5 L88,26.5"/>*/}
+              {/*        <path d="M13,50.5 L88,50.5"/>*/}
+              {/*        <path className="x_path" d="M13,50.5 L88,50.5"/>*/}
+              {/*        <path d="M13,74.5 L88,74.5"/>*/}
+              {/*      </g>*/}
+              {/*    </svg>*/}
+              {/*  </a>*/}
+              {/*</div>*/}
               <NewSideMenu />
             </header>
             <div
